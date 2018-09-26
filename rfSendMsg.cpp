089d75923784087f1c1a14b182af436bc0982c2f
Paul Jeffress <paul.jeffress@gmail.com>
2018-09-26 20:08:43 +1000

    SAMD GW Working RF-Eth ok
    Final code for now as everything is working.  The w5500.cpp/h changes were one of the biggest issues that took a while to resolve.  It was the use of interrupts that was causing lockups.

--- rfSendMsg.cpp
+++ rfSendMsg.cpp
@@ -23,19 +23,13 @@ void rfSendMsg(int rfPackTo) {
   #endif  // DEBUGPJ2
   
   Rstat = true;               // radio indicator on
-  digitalWrite(R_LED, HIGH);  // turn on radio LED
+  digitalWrite(R_LED, LOW);  // turn on radio LED
   onMillis = millis();        // store timestamp, so loop() can turn it off when the time is up.
 
   //RadioHead based sender code
   Serial.print("Sending to Mesh Addr: ");
   Serial.println(rfPackTo);  
 
-  // XXXX - copy mes into toSendData here XXXXXX
-  //uint8_t toSendData[] = "ABCDEFGHIJ";
-  //Serial.print("temp dummy toSendData =");
-  //Serial.println((char*)toSendData);
-  // Send a message to an rf69 mesh node
-  // A route to the destination will be automatically discovered.
   if (manager.sendtoWait((uint8_t*)&mes, sizeof(mes), rfPackTo) == RH_ROUTER_ERROR_NONE)
   {
     // It has been reliably delivered to the next node.
@@ -54,35 +48,7 @@ void rfSendMsg(int rfPackTo) {
       Serial.print("No connection with RF node:");
       Serial.println(rfPackTo);
     #endif
-  }
-  
-  // code below is the old pre RadioHead rf send code.
-  
-  // int i = RFTXRETRIES;                  // number of RF transmission retries
-
-  // while (rfToSend) && i>0) {       // first try to send packets
-  //   #ifdef DEBUGPJ
-  //     Serial.print(">>>rf-tx>>> to node:" );
-  //     Serial.println(target);
-  //   #endif
-  //   if (radio.sendWithRetry(target, (const void*)(&mes), sizeof(mes),RFTXRETRIES)) {
-  //     rfToSend = false;
-  //      } 
-  //   else delay(500);        // half a second delay between retries
-  //   i--;
-  //   } // end of while loop
-
-  // if (rfToSend && verbose) {          // if not succeeded in sending packets after 5 retries
-  //   sprintf(buff_topic, "home/sam_gw/nb/node%02d/dev90", NODEID); // construct MQTT topic and message
-  //   sprintf(buff_mess, "connection lost to node %d", target);    // for radio loss (device 90)
-  //   mqttClient.publish(buff_topic,buff_mess);     // publish ...
-  //   rfToSend = false;           // reset response needed flag
-  //   #ifdef DEBUGPJ2
-  //     Serial.print("No connection with RF node:");
-  //     Serial.println(target);
-  //   #endif
-  //   }
- 
+  } 
   rfToSend = false;       // reset send trigger
 
   Serial.println("rfSendMsg()- Done"); 
